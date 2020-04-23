2fc70bbe9bf1cd92210367619831e12455623a79
robert <robert@roberts-iMac-2.fritz.box>
2020-04-23 22:09:38 +0200

    first commit

---
+++ 02/test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8" />
+    <title>Hier steht der Titel</title>
+  </head>
+  <body>
+
+    <?php
+      $zahl1 = 28;
+      $zahl2 = 9;
+      $zahl3 = $zahl1 % $zahl2;
+      echo $zahl3;
+
+    ?>
+
+  </body>
+</html>
